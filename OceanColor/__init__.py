099aaa0feb7a950838af34df172a2f99de117322
Guilherme Castelão <guilherme@castelao.net>
2020-04-21 17:32:13 -0700

    Warning on missing environment variable for OSX
    Recent MacOSX require this environment variable set to YES to allow for
    parallel processing, which is used in OceanColor to manipulate files in
    the background.

--- OceanColor/__init__.py
+++ OceanColor/__init__.py
@@ -1,5 +1,16 @@
 """Top-level package for NASA Ocean Color."""
 
+import os
+import sys
+import warnings
+
+
 __author__ = """Guilherme Castelão"""
-__email__ = 'guilherme@castelao.net'
-__version__ = '0.0.2'
+__email__ = "guilherme@castelao.net"
+__version__ = "0.0.2"
+
+# Recent OSX requires this environment variable to run parallel processes
+if sys.platform == "darwin":
+    if os.environ.get("OBJC_DISABLE_INITIALIZE_FORK_SAFETY") != "YES":
+        msg = "You might require OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES"
+        warnings.warn(msg, RuntimeWarning)
