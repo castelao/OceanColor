d27ea26c36170c357d8c28bfdb00f96341d0df03
Guilherme Castelão <guilherme@castelao.net>
2020-12-23 14:57:16 -0800

    feat: test if OceanColorDB item is serializeable

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -3,6 +3,7 @@
 """Tests for `OceanColor` package."""
 
 import os
+import pickle
 import pytest
 
 from OceanColor.storage import parse_filename, OceanColorDB, FileSystem
@@ -30,3 +31,16 @@ def test_OceanColorDB():
     db.backend = FileSystem('./')
     ds = db['T2004006.L3m_DAY_CHL_chlor_a_4km.nc']
     ds.attrs
+
+
+def test_serialize_OceanColorDB():
+    """Test if a OceanColorDB item is serializeable
+
+    This is required to transport between processes, threads and queues.
+    """
+    db = OceanColorDB(os.getenv("NASA_USERNAME"), os.getenv("NASA_PASSWORD"))
+    db.backend = FileSystem('./')
+    ds = db['T2004006.L3m_DAY_CHL_chlor_a_4km.nc']
+    ds2 = pickle.loads(pickle.dumps(ds.compute()))
+
+    assert ds == ds2
