937aac4e9b56aed5aa9fe85a452665a2351fa72d
Guilherme Castelão <guilherme@castelao.net>
2020-12-23 17:35:28 -0800

    refactor: Moving from multiprocessing to threading
    Instead of using multiple processes it now operates with multiple
    threads.

--- OceanColor/OceanColor.py
+++ OceanColor/OceanColor.py
@@ -2,7 +2,7 @@
 
 from io import BytesIO
 import logging
-import multiprocessing as mp
+import threading, queue
 import os
 import time
 from typing import Any, Dict, Optional, Sequence
@@ -33,8 +33,8 @@ class InRange(object):
         if npes is None:
             npes = int(2 * mp.cpu_count())
         self.npes = npes
-        self.manager = mp.Manager()
-        self.queue = self.manager.Queue(int(3 * npes))
+        n_queue = int(3 * npes)
+        self.queue = queue.Queue(int(3 * npes))
 
         self.db = OceanColorDB(username, password)
         self.db.backend = FileSystem(path)
@@ -63,7 +63,7 @@ class InRange(object):
         dL_tol:
         """
         module_logger.debug("Searching for matchups.")
-        self.worker = mp.Process(
+        self.worker = threading.Thread(
             target=self.scanner,
             args=(self.queue, self.npes, track, sensor, dtype, dt_tol, dL_tol),
         )
@@ -77,27 +77,29 @@ class InRange(object):
         filenames = bloom_filter(track, sensor, dtype, dt_tol, dL_tol)
         module_logger.debug("Finished bloom filter")
 
-        with mp.Pool(processes=npes) as pool:
-            results = []
-            for f in filenames:
-                module_logger.debug("New target: {}".format(f))
-                if len(results) > npes:
-                    idx = [r.ready() for r in results]
-                    while not np.any(idx):
-                        time.sleep(1)
-                        idx = [r.ready() for r in results]
-                    tmp = results.pop(idx.index(True)).get()
-                    module_logger.debug("Finished reading another file")
-                    if not tmp.empty:
-                        module_logger.info("Found {} matchs".format(len(tmp)))
-                        queue.put(tmp)
-                module_logger.debug("Getting {}".format(f["filename"]))
-                ds = self.db[f["filename"]].compute()
-                results.append(pool.apply_async(inrange, (track, ds, dL_tol, dt_tol)))
-            for tmp in (r.get(timeout) for r in results):
-                module_logger.debug("Finished reading another file")
-                if not tmp.empty:
-                    module_logger.info("Found {} matchs".format(len(tmp)))
-                    queue.put(tmp)
+        results = []
+        for f in filenames:
+            module_logger.info("Scanning: {}".format(f))
+            if len(results) > 2:
+                idx = [r.is_alive() for r in results]
+                if np.all(idx):
+                    r = results.pop(0)
+                    module_logger.debug("Waiting for {}".format(r.name))
+                else:
+                    r =  results.pop(idx.index(False))
+                r.join()
+                module_logger.debug("Finished {}".format(r.name))
+            module_logger.debug("Getting {}".format(f))
+            ds = self.db[f].compute()
+            module_logger.debug("Launching search on {}".format(f))
+            results.append(
+                threading.Thread(
+                    target=inrange,
+                    args=(track, ds, dL_tol, dt_tol, queue)))
+            results[-1].start()
+        for r in results:
+            r.join()
+            module_logger.debug("Finished {}".format(r.name))
+
         module_logger.debug("Finished scanning all potential matchups.")
         queue.put("END")
