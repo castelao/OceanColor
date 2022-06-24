b39f3d1991a6362b0d38c400d22cc702452d24e3
Guilherme Castelão <guilherme@castelao.net>
2022-06-24 15:44:31 -0700

    Updating minimum requirements (#28)
    * Cleaning setup.cfg
    
    * Cleaning setup.py, moving metadata to pyproject.toml
    
    * Updating depencies requirements
    
    * No guarantees for Python-3.7 anymore
    
    * Update checkout actions
    
    * Guarantee numpy >= 1.21
    
    * No guarantees for Python-3.7 anymore
    
    * Cleaning redundancy on setup.py
    
    * Cleaning setup.py as much as possible
    
    * Updating environment for notebooks
    
    * Testing up to loky-2.9
    
    * Minimum pip to verify
    
    * Updating matrix of TOX tests

--- setup.py
+++ setup.py
@@ -2,28 +2,6 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
-def requirements():
-    with open('requirements.txt') as f:
-        return f.read()
-
-setup(
-    entry_points={
-        'console_scripts': [
-            'OceanColor=OceanColor.cli:main',
-        ],
-    },
-    install_requires=requirements(),
-    long_description=readme + '\n\n' + history,
-    include_package_data=True,
-    name='OceanColor',
-    packages=find_packages(include=['OceanColor', 'OceanColor.*']),
-    zip_safe=False,
-)
+setup()
